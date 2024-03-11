6779d7a82b8b35a0fcc7535fece9750ffe2e9a1a
AlexanderLe <a.p.a.le@students.uu.nl>
2024-03-11 20:46:21 +0100

    mannnnn ik heb zoveel geprobeerd en verwijderd en ik ga dood van binnen

--- script.js
+++ script.js
@@ -1,164 +1,174 @@
 class Person {
     #name;
     #yearOfBirth;
-  
+
     constructor(name, yearOfBirth) {
-      this.#name = name;
-      this.#yearOfBirth = yearOfBirth;
+        this.#name = name;
+        this.#yearOfBirth = yearOfBirth;
     }
-  
+
     get name() {
-      return this.#name;
+        return this.#name;
     }
-  
+
     get yearOfBirth() {
-      return this.#yearOfBirth;
+        return this.#yearOfBirth;
     }
-  }
-  
-  class Author extends Person {
+}
+
+class Author extends Person {
     #titles;
     #wikipediaLink;
-  
+
     constructor(name, yearOfBirth, titles, wikipediaLink) {
-      super(name, yearOfBirth);
-      this.#titles = titles;
-      this.#wikipediaLink = wikipediaLink;
+        super(name, yearOfBirth);
+        this.#titles = titles;
+        this.#wikipediaLink = wikipediaLink;
     }
-  
+
     get titles() {
-      return this.#titles;
+        return this.#titles;
     }
-  
+
     get wikipediaLink() {
-      return this.#wikipediaLink;
+        return this.#wikipediaLink;
     }
-  }
-  
-  class Company {
+}
+
+class Company {
     #name;
     #wikipediaLink;
-  
+
     constructor(name, wikipediaLink) {
-      this.#name = name;
-      this.#wikipediaLink = wikipediaLink;
+        this.#name = name;
+        this.#wikipediaLink = wikipediaLink;
     }
-  
+
     get name() {
-      return this.#name;
+        return this.#name;
     }
-  
+
     get wikipediaLink() {
-      return this.#wikipediaLink;
+        return this.#wikipediaLink;
     }
-  }
-  
-  class Publisher extends Company {
+}
+
+class Publisher extends Company {
     #titles;
-  
+
     constructor(name, wikipediaLink, titles) {
-      super(name, wikipediaLink);
-      this.#titles = titles;
+        super(name, wikipediaLink);
+        this.#titles = titles;
     }
-  
+
     get titles() {
-      return this.#titles;
+        return this.#titles;
     }
-  }
-  
-  class CreativeWork {
+}
+
+class CreativeWork {
     #authors;
     #year;
     #title;
-  
+
     constructor(authors, year, title) {
-      this.#authors = authors;
-      this.#year = year;
-      this.#title = title;
+        this.#authors = authors;
+        this.#year = year;
+        this.#title = title;
     }
-  
+
     get authors() {
-      return this.#authors;
+        return this.#authors;
     }
-  
+
     get year() {
-      return this.#year;
+        return this.#year;
     }
-  
+
     get title() {
-      return this.#title;
+        return this.#title;
     }
-  }
-  
-  class Book extends CreativeWork {
+}
+
+class Book extends CreativeWork {
     #genre;
     #publisher;
     #cover;
     #plot;
-  
+
     constructor(authors, year, title, genre, publisher, cover, plot) {
-      super(authors, year, title);
-      this.#genre = genre;
-      this.#publisher = publisher;
-      this.#cover = cover;
-      this.#plot = plot;
+        super(authors, year, title);
+        this.#genre = genre;
+        this.#publisher = publisher;
+        this.#cover = cover;
+        this.#plot = plot;
     }
-  
+
     get genre() {
-      return this.#genre;
+        return this.#genre;
     }
-  
+
     get publisher() {
-      return this.#publisher;
+        return this.#publisher;
     }
-  
+
     get cover() {
-      return this.#cover;
+
+        return this.#cover;
     }
-  
+
     get plot() {
-      return this.#plot;
+        return this.#plot;
     }
-  }
-let author = new Author("George R. R. Martin", 1948, ["A Game of Thones"], "https://en.wikipedia.org/wiki/George_R._R._Martin");
-let publisher = new Publisher("Luitingh-Sijthoff", "https://nl.wikipedia.org/wiki/Luitingh-Sijthoff", ["A Game of Thones", "A Clash of Kings", "A Storm of Swords", "A Feast for Crows", "A Dance with Dragons", "The Winds of Winter", "A Dream of Spring"]);
+}
 
-let book = new Book([author], 1996, "A Game of Thrones", "Fantasy", publisher, "https://upload.wikimedia.org/wikipedia/en/d/d8/A_Game_of_Thrones.jpg", "The book is set in the fictional Seven Kingdoms of Westeros and the continent of Essos, and follows the noble families fighting for the Iron Throne and control of Westeros.");
-
+let author = new Author("George R. R. Martin", 1948, ["A Game of Thrones"], "https://en.wikipedia.org/wiki/George_R._R._Martin");
+let publisher = new Publisher("Luitingh-Sijthoff", "https://nl.wikipedia.org/wiki/Luitingh-Sijthoff", ["A Game of Thrones", "A Clash of Kings", "A Storm of Swords", "A Feast for Crows", "A Dance with Dragons", "The Winds of Winter", "A Dream of Spring"]);
+let book = new Book([author], 1996, "A Game of Thrones", "Fantasy", publisher, "/pictures/authorpage/book1.jpg",
+    "The Winds of Winterfell\n" +
+    "In the shadow of the ancient castle of Winterfell, the Stark family stands resolute. Eddard “Ned” Stark, the stoic lord, receives a summons that will alter the course of his life. His old friend, King Robert Baratheon, beckons him to the capital, King’s Landing, to serve as Hand of the King. Ned’s honor compels him to accept, though he senses treachery lurking in the shadows.\n" +
+    "\n" +
+    "Lannisters and Lions\n" +
+    "Across the realm, the Lannister lions prowl. Queen Cersei, with her golden hair and silver tongue, conceals secrets like daggers beneath her velvet gowns. Her twin brother, Jaime, the Kingslayer, shares both her blood and her forbidden desires. When young Bran Stark stumbles upon their clandestine tryst, the world tilts on its axis. Jaime’s desperate act—pushing Bran from a tower window—ripples through time, leaving scars on hearts and kingdoms.\n" +
+    "\n" +
+    "The Exiled Princess\n" +
+    "Beyond the Narrow Sea, Daenerys Targaryen, the last scion of the deposed Targaryen dynasty, marries Khal Drogo, a fierce warrior of the nomadic Dothraki. Their union is a fragile alliance, sealed with dragon eggs—the remnants of a lost age. Daenerys, once a pawn, begins to awaken. Her journey from meek bride to Mother of Dragons will echo across continents, igniting flames that threaten to consume all.\n" +
+    "\n" +
+    "The Iron Throne Beckons\n" +
+    "As winter looms, the Iron Throne remains the ultimate prize. Noble houses clash, alliances fray, and whispers of ancient magic resurface. Ned Stark navigates treacherous court politics, unaware that his honor may cost him dearly. Meanwhile, Jon Snow, Ned’s bastard son, forsakes his birthright to join the Night’s Watch—a brotherhood sworn to guard the Wall against the terrors beyond.\n" +
+    "\n" +
+    "Intrigue, Betrayal, and Destiny\n" +
+    "In this game of thrones, no one is safe. Schemes intertwine like ivy on castle walls. The direwolves howl, and the raven’s wings bear secrets. Amidst the clash of swords and the rustle of silken gowns, destinies converge. Winter is coming, and with it, a storm that will reshape the Seven Kingdoms forever.\n" +
+    "\n" +
+    "And so, dear reader, whether you stand in the icy winds of the North or bask in the warmth of King’s Landing, remember that every choice echoes through time, and the game of thrones plays on, heedless of mortal hearts");
 
 
 // This function will run when the page loads
-window.onload = function() {
+window.onload = function () {
     console.log("The page has loaded!");
 
-   
+    // Create elements for the book's information
 
+    let titleElement = document.createElement('h1');
+    titleElement.textContent = book.title;
 
+    let genreElement = document.createElement('p');
+    genreElement.textContent = `Genre: ${book.genre}`;
 
+    let yearElement = document.createElement('p');
+    yearElement.textContent = `Year: ${book.year}`;
 
+    let authorElement = document.createElement('p');
+    authorElement.textContent = `Author: ${author.name}`;
 
-  // Create elements for the book's information
+    let publisherElement = document.createElement('p');
+    publisherElement.textContent = `Publisher: ${publisher.name}`;
 
-  let titleElement = document.createElement('h1');
-  titleElement.textContent = book.title;
+    let coverElement = document.createElement('img');
+    coverElement.src = book.cover;
 
-  let genreElement = document.createElement('p');
-  genreElement.textContent = `Genre: ${book.genre}`;
-
-  let yearElement = document.createElement('p');
-  yearElement.textContent = `Year: ${book.year}`;
-
-  let authorElement = document.createElement('p');
-  authorElement.textContent = `Author: ${author.name}`;
-
-  let publisherElement = document.createElement('p');
-  publisherElement.textContent = `Publisher: ${publisher.name}`;
-
-  let coverElement = document.createElement('img');
-  coverElement.src = book.cover;
-
-  let plotElement = document.createElement('p');
-  plotElement.textContent = `Plot: ${book.plot}`;
+    let plotElement = document.createElement('p');
+    plotElement.textContent = `${book.plot}`;
 
     // Append these elements to the main element
     let mainElement = document.querySelector('main');
